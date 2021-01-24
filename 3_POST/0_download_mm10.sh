36a206ad5333fc2421c4f9d66b054dbcd5904aac
Woojin Yang <wugeneyang@gmail.com>
2021-01-24 11:40:02 +0300

    Post proc

---
+++ 3_POST/0_download_mm10.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget https://hgdownload.soe.ucsc.edu/goldenPath/mm10/bigZips/genes/mm10.ensGene.gtf.gz
+gunzip mm10.ensGene.gtf.gz
---
+++ 3_POST/0_download_scNMT_OUT.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+wget https://raw.github.com/circulosmeos/gdown.pl/master/gdown.pl
+chmod u+x gdown.pl
+./gdown.pl 'https://drive.google.com/file/d/1tQT_vx0SmWwf9oNJDm1YmfCDrqOb48O0/view?usp=sharing' TR_OUTPUT_scNMT.tar.gz
+tar xvfz TR_OUTPUT_scNMT.tar.gz
+
+
